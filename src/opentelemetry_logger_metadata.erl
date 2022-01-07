dec8a6c93b9d9e91bc619c66b706ec3777ce050f
Indrek Juhkam <indrek@urgas.eu>
2022-01-07 14:59:33 +0200

    Upgrade opentelemetry to latest stable version

--- src/opentelemetry_logger_metadata.erl
+++ src/opentelemetry_logger_metadata.erl
@@ -15,14 +15,8 @@ filter(LogEvent, _Config) ->
         undefined ->
             ignore;
         SpanCtx ->
-            TraceId = otel_span:trace_id(SpanCtx),
-            SpanId = otel_span:span_id(SpanCtx),
-
-            %% hex_trace_id and hex_span_id can be used after
-            %% https://github.com/open-telemetry/opentelemetry-erlang/commit/a97f5b7f
-            %% is released
-            HexTraceId = iolist_to_binary(io_lib:format("~32.16.0b", [TraceId])),
-            HexSpanId = iolist_to_binary(io_lib:format("~16.16.0b", [SpanId])),
+            HexTraceId = otel_span:hex_trace_id(SpanCtx),
+            HexSpanId = otel_span:hex_span_id(SpanCtx),
 
             #{meta := Meta} = LogEvent,
             MetaWithTraceInformation = Meta#{trace_id => HexTraceId, span_id => HexSpanId},
